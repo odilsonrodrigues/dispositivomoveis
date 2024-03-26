87e7b702eaefb4288d5c4270076ba343c3b2c26e
unknown <odilson.rodrigues@estudante.ifms.edu.br>
2024-03-25 20:46:24 -0400

    Commitando

---
+++ src/pages/Bebidas.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from "react-native";
+
+function bebidasScreen() {
+    return (
+        <View>
+            <Text>bebidas</Text>
+        </View>
+    );
+}
+
+export default bebidasScreen;
